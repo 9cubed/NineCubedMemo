5fafd12c28992815d7dd0140091ae3116824f342
9cubed <38031020+9cubed@users.noreply.github.com>
2018-04-03 23:32:09 +0900

    初回コミット

---
+++ src/NineCubedMemo/NineCubedMemo/Memo/MainForm.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace NineCubed.Memo
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.txtMain = new System.Windows.Forms.RichTextBox();
+            this.menuBar = new System.Windows.Forms.MenuStrip();
+            this.menuFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuFile_New = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuFile_Open = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuFile_Save = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuFile_SaveAs = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuFile_Close = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuFile_End = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuEdit = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuEdit_Undo = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuEdit_Redo = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuEdit_Cut = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuEdit_Copy = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuEdit_Paste = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuEdit_Delete = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolFile_New = new System.Windows.Forms.ToolStripButton();
+            this.toolFile_Open = new System.Windows.Forms.ToolStripButton();
+            this.toolFile_Save = new System.Windows.Forms.ToolStripButton();
+            this.menuHelp = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuHelp_version = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuBar.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtMain
+            // 
+            this.txtMain.Location = new System.Drawing.Point(22, 78);
+            this.txtMain.Name = "txtMain";
+            this.txtMain.Size = new System.Drawing.Size(297, 152);
+            this.txtMain.TabIndex = 0;
+            this.txtMain.Text = "";
+            // 
+            // menuBar
+            // 
+            this.menuBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuFile,
+            this.menuEdit,
+            this.menuHelp});
+            this.menuBar.Location = new System.Drawing.Point(0, 0);
+            this.menuBar.Name = "menuBar";
+            this.menuBar.Size = new System.Drawing.Size(579, 24);
+            this.menuBar.TabIndex = 1;
+            this.menuBar.Text = "menuStrip1";
+            // 
+            // menuFile
+            // 
+            this.menuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuFile_New,
+            this.menuFile_Open,
+            this.menuFile_Save,
+            this.menuFile_SaveAs,
+            this.menuFile_Close,
+            this.menuFile_End});
+            this.menuFile.Name = "menuFile";
+            this.menuFile.Size = new System.Drawing.Size(53, 20);
+            this.menuFile.Text = "ファイル";
+            // 
+            // menuFile_New
+            // 
+            this.menuFile_New.Name = "menuFile_New";
+            this.menuFile_New.Size = new System.Drawing.Size(161, 22);
+            this.menuFile_New.Text = "新規作成";
+            // 
+            // menuFile_Open
+            // 
+            this.menuFile_Open.Name = "menuFile_Open";
+            this.menuFile_Open.Size = new System.Drawing.Size(161, 22);
+            this.menuFile_Open.Text = "開く";
+            // 
+            // menuFile_Save
+            // 
+            this.menuFile_Save.Name = "menuFile_Save";
+            this.menuFile_Save.Size = new System.Drawing.Size(161, 22);
+            this.menuFile_Save.Text = "上書き保存";
+            // 
+            // menuFile_SaveAs
+            // 
+            this.menuFile_SaveAs.Name = "menuFile_SaveAs";
+            this.menuFile_SaveAs.Size = new System.Drawing.Size(161, 22);
+            this.menuFile_SaveAs.Text = "名前を付けて保存";
+            // 
+            // menuFile_Close
+            // 
+            this.menuFile_Close.Name = "menuFile_Close";
+            this.menuFile_Close.Size = new System.Drawing.Size(161, 22);
+            this.menuFile_Close.Text = "閉じる";
+            // 
+            // menuFile_End
+            // 
+            this.menuFile_End.Name = "menuFile_End";
+            this.menuFile_End.Size = new System.Drawing.Size(161, 22);
+            this.menuFile_End.Text = "終了";
+            // 
+            // menuEdit
+            // 
+            this.menuEdit.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuEdit_Undo,
+            this.menuEdit_Redo,
+            this.menuEdit_Cut,
+            this.menuEdit_Copy,
+            this.menuEdit_Paste,
+            this.menuEdit_Delete});
+            this.menuEdit.Name = "menuEdit";
+            this.menuEdit.Size = new System.Drawing.Size(43, 20);
+            this.menuEdit.Text = "編集";
+            // 
+            // menuEdit_Undo
+            // 
+            this.menuEdit_Undo.Name = "menuEdit_Undo";
+            this.menuEdit_Undo.Size = new System.Drawing.Size(117, 22);
+            this.menuEdit_Undo.Text = "元に戻す";
+            // 
+            // menuEdit_Redo
+            // 
+            this.menuEdit_Redo.Name = "menuEdit_Redo";
+            this.menuEdit_Redo.Size = new System.Drawing.Size(117, 22);
+            this.menuEdit_Redo.Text = "やり直す";
+            // 
+            // menuEdit_Cut
+            // 
+            this.menuEdit_Cut.Name = "menuEdit_Cut";
+            this.menuEdit_Cut.Size = new System.Drawing.Size(117, 22);
+            this.menuEdit_Cut.Text = "切り取り";
+            // 
+            // menuEdit_Copy
+            // 
+            this.menuEdit_Copy.Name = "menuEdit_Copy";
+            this.menuEdit_Copy.Size = new System.Drawing.Size(117, 22);
+            this.menuEdit_Copy.Text = "コピー";
+            // 
+            // menuEdit_Paste
+            // 
+            this.menuEdit_Paste.Name = "menuEdit_Paste";
+            this.menuEdit_Paste.Size = new System.Drawing.Size(117, 22);
+            this.menuEdit_Paste.Text = "貼り付け";
+            // 
+            // menuEdit_Delete
+            // 
+            this.menuEdit_Delete.Name = "menuEdit_Delete";
+            this.menuEdit_Delete.Size = new System.Drawing.Size(117, 22);
+            this.menuEdit_Delete.Text = "削除";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolFile_New,
+            this.toolFile_Open,
+            this.toolFile_Save});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(579, 25);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolFile_New
+            // 
+            this.toolFile_New.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolFile_New.Image = ((System.Drawing.Image)(resources.GetObject("toolFile_New.Image")));
+            this.toolFile_New.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolFile_New.Name = "toolFile_New";
+            this.toolFile_New.Size = new System.Drawing.Size(23, 22);
+            this.toolFile_New.Text = "toolStripButton1";
+            // 
+            // toolFile_Open
+            // 
+            this.toolFile_Open.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolFile_Open.Image = ((System.Drawing.Image)(resources.GetObject("toolFile_Open.Image")));
+            this.toolFile_Open.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolFile_Open.Name = "toolFile_Open";
+            this.toolFile_Open.Size = new System.Drawing.Size(23, 22);
+            this.toolFile_Open.Text = "toolStripButton1";
+            // 
+            // toolFile_Save
+            // 
+            this.toolFile_Save.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolFile_Save.Image = ((System.Drawing.Image)(resources.GetObject("toolFile_Save.Image")));
+            this.toolFile_Save.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolFile_Save.Name = "toolFile_Save";
+            this.toolFile_Save.Size = new System.Drawing.Size(23, 22);
+            this.toolFile_Save.Text = "toolStripButton1";
+            // 
+            // menuHelp
+            // 
+            this.menuHelp.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuHelp_version});
+            this.menuHelp.Name = "menuHelp";
+            this.menuHelp.Size = new System.Drawing.Size(48, 20);
+            this.menuHelp.Text = "ヘルプ";
+            // 
+            // menuHelp_version
+            // 
+            this.menuHelp_version.Name = "menuHelp_version";
+            this.menuHelp_version.Size = new System.Drawing.Size(180, 22);
+            this.menuHelp_version.Text = "バージョン情報";
+            this.menuHelp_version.Click += new System.EventHandler(this.menuHelp_version_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(579, 344);
+            this.Controls.Add(this.txtMain);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.menuBar);
+            this.MainMenuStrip = this.menuBar;
+            this.Name = "MainForm";
+            this.Text = "メモ帳";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.menuBar.ResumeLayout(false);
+            this.menuBar.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtMain;
+        private System.Windows.Forms.MenuStrip menuBar;
+        private System.Windows.Forms.ToolStripMenuItem menuFile;
+        private System.Windows.Forms.ToolStripMenuItem menuFile_New;
+        private System.Windows.Forms.ToolStripMenuItem menuFile_Open;
+        private System.Windows.Forms.ToolStripMenuItem menuFile_Save;
+        private System.Windows.Forms.ToolStripMenuItem menuFile_SaveAs;
+        private System.Windows.Forms.ToolStripMenuItem menuFile_Close;
+        private System.Windows.Forms.ToolStripMenuItem menuFile_End;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolFile_New;
+        private System.Windows.Forms.ToolStripButton toolFile_Open;
+        private System.Windows.Forms.ToolStripButton toolFile_Save;
+        private System.Windows.Forms.ToolStripMenuItem menuEdit;
+        private System.Windows.Forms.ToolStripMenuItem menuEdit_Undo;
+        private System.Windows.Forms.ToolStripMenuItem menuEdit_Redo;
+        private System.Windows.Forms.ToolStripMenuItem menuEdit_Cut;
+        private System.Windows.Forms.ToolStripMenuItem menuEdit_Copy;
+        private System.Windows.Forms.ToolStripMenuItem menuEdit_Paste;
+        private System.Windows.Forms.ToolStripMenuItem menuEdit_Delete;
+        private System.Windows.Forms.ToolStripMenuItem menuHelp;
+        private System.Windows.Forms.ToolStripMenuItem menuHelp_version;
+    }
+}
---
+++ src/NineCubedMemo/NineCubedMemo/Memo/MainForm.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NineCubed.Memo
+{
+    public partial class MainForm : Form
+    {
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// MainForm の Load イベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            //テキストボックスを初期化します
+            initTextBox(txtMain, 12);
+
+            //隙間がなくなるようにテキストボックスを配置します
+            txtMain.Dock = DockStyle.Fill;
+        }
+
+        /// <summary>
+        /// テキストボックスを初期化します
+        /// </summary>
+        /// <param name="textBox">初期化するテキストボックス</param>
+        private void initTextBox(RichTextBox textBox, float fontSize)
+        {
+            //折り返さないようにします
+            textBox.WordWrap = false;
+
+            //タブを入力できるようにします。false にすると、別のコントロールにフォーカスが移動してしまい、タブを入力できません
+            textBox.AcceptsTab = true;
+
+            //文字の幅が同じするため、等幅フォントの「ＭＳ ゴシック」にします
+            textBox.Font = new Font("ＭＳ ゴシック", fontSize);
+
+            //デフォルトでは、半角英数字は英語フォント、それ以外は日本語フォントを使う DualFont になっているため、AutoFont だけを再設定します
+            //この設定をしないと、半角の「i」の幅がものすごく狭くなってしまう
+            textBox.LanguageOption = RichTextBoxLanguageOptions.AutoFont;
+
+            //テキストボックスの左の内側に余白を設定します
+            textBox.SelectionIndent = 6;
+
+            //URLが入力された時に書式が変わらないようにする
+            textBox.DetectUrls = false; //true にすると、LinkClickedイベントでクリックされたURL(e.LinkText)が取得できる
+
+            //タブの位置を設定します
+            setSelectionTabs(textBox, 4);
+        }
+
+        /// <summary>
+        /// テキストボックスにタブの停止位置を設定します
+        /// 
+        /// タブの停止位置は、1タブ＝半角スペース4個分、などのように指定できません
+        /// 実際に描画される1文字分の幅を取得して、そこからタブが止まる位置を計算して設定しています
+        /// </summary>
+        /// <param name="textBox">テキストボックス</param>
+        /// <param name="tabSize">1タブあたりの文字数</param>
+        private void setSelectionTabs(RichTextBox textBox, int tabSize)
+        {
+            //1文字の幅のサイズを取得します
+            int charWidth = 0;
+            using (Graphics g = this.CreateGraphics()) {
+                Size size = TextRenderer.MeasureText(g, "A", textBox.Font, new Size(int.MaxValue, int.MaxValue), TextFormatFlags.NoPadding);
+                charWidth = size.Width;
+            }
+
+            //タブで止まる位置を配列に設定します
+            int[] tabArray = new int[32]; //32より多く設定するとエラーとなる
+            for (int i = 0; i < tabArray.Count(); i++)
+            {
+                tabArray[i] = (charWidth * tabSize * (i + 1) + textBox.SelectionIndent); //textBox.SelectionIndent はテキスト内の左側の余白サイズ
+            }
+
+            //タブで止まる位置を設定します
+            textBox.SelectionTabs = tabArray;
+        }
+
+
+        /// <summary>
+        /// メニュー・バージョン情報 の Clickイベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void menuHelp_version_Click(object sender, EventArgs e)
+        {
+            //バージョン情報を取得します
+            FileVersionInfo fileVersionInfo = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location);
+            string version =
+                fileVersionInfo.ProductName    + " "  + //製品名
+                fileVersionInfo.ProductVersion + "\n" + //製品バージョン
+                fileVersionInfo.LegalCopyright;         //コピーライト
+
+            //バージョン情報を表示します
+            MessageBox.Show(version);
+        }
+
+    } //class
+}
