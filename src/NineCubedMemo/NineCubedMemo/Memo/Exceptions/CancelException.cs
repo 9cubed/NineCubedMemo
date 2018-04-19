81ac745ccb9d39ada3bfa1e14a713cc1e23e8b00
9cubed <38031020+9cubed@users.noreply.github.com>
2018-04-19 18:02:50 +0900

    追加忘れ。

---
+++ src/NineCubedMemo/NineCubedMemo/Memo/Exceptions/CancelException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NineCubed.Memo.Exceptions
+{
+    public class CancelException : Exception
+    {
+    }
+}
