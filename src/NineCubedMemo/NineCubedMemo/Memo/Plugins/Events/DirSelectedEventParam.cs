8051e3663fa79507461c3e6a1dc624757ff27a20
9cubed <38031020+9cubed@users.noreply.github.com>
2018-10-28 10:14:13 +0900

    ファイル漏れ。。。

---
+++ src/NineCubedMemo/NineCubedMemo/Memo/Plugins/Events/DirSelectedEventParam.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NineCubed.Memo.Plugins.Events
+{
+
+    public class DirSelectedEventParam : EventParam
+    {
+        //イベント名
+        public static string Name = "PluginEvent_DirSelected";
+
+        //選択されたディレクトリのパス
+        public string Path { get; set; }
+    }
+
+}
